b6de60cd7872b5feae42d303f47e6a9576bc4d39
NoirAct <daniel_sousa1308@hotmail.com>
2024-07-20 01:06:26 -0300

    feat: new sizes and align items middle

--- src/pages/Placar/index.js
+++ src/pages/Placar/index.js
@@ -1,106 +1,128 @@
-import React, { useState, useEffect } from 'react'
-import './Placar.css'
+import React, { useState, useEffect } from "react";
+import "./Placar.css";
 
-export default function PlacarOnline(props){
-  
-  const [TypePlacar,setTypePlacar] = useState("config")
-  
+export default function PlacarOnline(props) {
+  const [TypePlacar, setTypePlacar] = useState("config");
+
   const initialName = {
-    timeA: 'Time A',
-    timeB: 'Time B'
-  }
-  const [NomeTime, setNomeTime] = useState(initialName)
+    timeA: "Time A",
+    timeB: "Time B",
+  };
+  const [NomeTime, setNomeTime] = useState(initialName);
 
+  const [GolsTimeA, setGolsTimeA] = useState(0);
+  const [GolsTimeB, setGolsTimeB] = useState(0);
 
-  const [GolsTimeA, setGolsTimeA] = useState(0)
-  const [GolsTimeB, setGolsTimeB] = useState(0)
-
-  const [Timer, setTimer] = useState(0)
-  const [TimerOn, setTimerOn] = useState(false)
-
+  const [Timer, setTimer] = useState(0);
+  const [TimerOn, setTimerOn] = useState(false);
 
   useEffect(() => {
-    let interval = null
+    let interval = null;
 
-    if(TimerOn){
+    if (TimerOn) {
       interval = setInterval(() => {
-          setTimer(prevTime => prevTime + 100)
-        },100)
-      }else{
-        clearInterval(interval)
-      }
+        setTimer((prevTime) => prevTime + 100);
+      }, 100);
+    } else {
+      clearInterval(interval);
+    }
 
-      return () => clearInterval(interval)
+    return () => clearInterval(interval);
+  }, [TimerOn]);
 
-    }, [TimerOn])
-  
-  if ( TypePlacar === "config") {
-    return configPlacar()
+  if (TypePlacar === "config") {
+    return configPlacar();
   }
-  if ( TypePlacar === "placar" ) {
-    return placar (NomeTime.timeA, NomeTime.timeB)
+  if (TypePlacar === "placar") {
+    return placar(NomeTime.timeA, NomeTime.timeB);
   }
 
-  function configPlacar() { 
+  function configPlacar() {
     function nomeiaTime(ev) {
-      const { name, value } = ev.target
-      setNomeTime({ ...NomeTime, [name]: value})
+      const { name, value } = ev.target;
+      setNomeTime({ ...NomeTime, [name]: value });
     }
 
     function handleSubmit(ev) {
-      ev.preventDefault()
-      setTypePlacar("placar")
+      ev.preventDefault();
+      setTypePlacar("placar");
     }
-  
-    return(
+
+    return (
       <div className="config-placar__container">
         <div className="title__container">
           <h1>Insira as informações da partida</h1>
         </div>
         <form className="info-partida-form" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Nome do time A" name="timeA" id="timeA" onChange={nomeiaTime}/>
-          <input type="text" placeholder="Nome do time B" name="timeB" id="timeB" onChange={nomeiaTime}/>
-          <button id="start" type="submit">Iniciar Partida!</button>
+          <input
+            type="text"
+            placeholder="Nome do time A"
+            name="timeA"
+            id="timeA"
+            onChange={nomeiaTime}
+          />
+          <input
+            type="text"
+            placeholder="Nome do time B"
+            name="timeB"
+            id="timeB"
+            onChange={nomeiaTime}
+          />
+          <button id="start" type="submit">
+            Iniciar Partida!
+          </button>
         </form>
       </div>
-    )
+    );
   }
 
   function placar(timeA, timeB) {
-    function anularGolsTimeA(){
-      if(GolsTimeA > 0){
-        setGolsTimeA(GolsTimeA - 1)
+    function anularGolsTimeA() {
+      if (GolsTimeA > 0) {
+        setGolsTimeA(GolsTimeA - 1);
       }
     }
 
-    function anularGolsTimeB(){
-      if(GolsTimeB > 0){
-        setGolsTimeB(GolsTimeB - 1)
-      } 
+    function anularGolsTimeB() {
+      if (GolsTimeB > 0) {
+        setGolsTimeB(GolsTimeB - 1);
+      }
     }
 
     function confirmarReset() {
-      let confirmReset = window.confirm("Deseja realmente reiniciar?")
-      if(confirmReset){
-        resetPlacar()
+      let confirmReset = window.confirm("Deseja realmente reiniciar?");
+      if (confirmReset) {
+        resetTempo();
+      }
+    }
+    function confirmarResetAll() {
+      let confirmReset = window.confirm(
+        "Deseja realmente iniciar nova partida?"
+      );
+      if (confirmReset) {
+        resetAll();
       }
     }
 
+    function resetTempo() {
+      setTimer(0);
+      setTimerOn(false);
+    }
+
     function resetPlacar() {
-      setTimer(0)
-      setTimerOn(false)
-      setGolsTimeA(0)
-      setGolsTimeB(0)
+      setTimer(0);
+      setTimerOn(false);
+      setGolsTimeA(0);
+      setGolsTimeB(0);
     }
 
     function resetAll() {
-      setTypePlacar("config")
-      resetPlacar()
+      setTypePlacar("config");
+      resetPlacar();
     }
 
-    return(
+    return (
       <div className="container">
-
         <div className="placar__container">
           <div className="placar__time a">
             <h1>{timeA}</h1>
@@ -112,9 +134,23 @@ export default function PlacarOnline(props){
               <span>{("0" + Math.floor((Timer / 60000) % 60)).slice(-2)}:</span>
               <span>{("0" + Math.floor((Timer / 1000) % 60)).slice(-2)}</span>
             </div>
-            <button type="button" id="btnStart" onClick={() => setTimerOn(true)}>Começar</button>
-            <button type="button" id="btnPause" onClick={() => setTimerOn(false)}>Pausar</button>
-            <button type="button" id="btnResume" onClick={confirmarReset}>Reiniciar</button>
+            <button
+              type="button"
+              id="btnStart"
+              onClick={() => setTimerOn(true)}
+            >
+              Começar
+            </button>
+            <button
+              type="button"
+              id="btnPause"
+              onClick={() => setTimerOn(false)}
+            >
+              Pausar
+            </button>
+            <button type="button" id="btnResume" onClick={confirmarReset}>
+              Reiniciar
+            </button>
           </div>
 
           <div className="placar__time b">
@@ -125,21 +161,46 @@ export default function PlacarOnline(props){
 
         <div className="placar__reset">
           <div className="marcar-button">
-            <button type="button" className="gol-validado" onClick={()=> setGolsTimeA(GolsTimeA + 1)}>Goool</button>
-            <button type="button" className="gol-cancelado" onClick={anularGolsTimeA}>Cancelar Gol</button>
+            <button
+              type="button"
+              className="gol-validado"
+              onClick={() => setGolsTimeA(GolsTimeA + 1)}
+            >
+              Goool
+            </button>
+            <button
+              type="button"
+              className="gol-cancelado"
+              onClick={anularGolsTimeA}
+            >
+              Cancelar Gol
+            </button>
           </div>
-          
+
           <div className="reset-button">
-            <button type="button" onClick={resetAll}>Iniciar novo jogo</button>
-            <button type="button" onClick={resetPlacar}>Reiniciar Partida</button>
+            <button type="button" onClick={confirmarResetAll}>
+              Iniciar novo jogo
+            </button>
           </div>
 
           <div className="marcar-button">
-            <button type="button" className="gol-validado" onClick={()=> setGolsTimeB(GolsTimeB + 1)}>Goool</button>
-            <button type="button" className="gol-cancelado" onClick={anularGolsTimeB}>Cancelar Gol</button>
+            <button
+              type="button"
+              className="gol-validado"
+              onClick={() => setGolsTimeB(GolsTimeB + 1)}
+            >
+              Goool
+            </button>
+            <button
+              type="button"
+              className="gol-cancelado"
+              onClick={anularGolsTimeB}
+            >
+              Cancelar Gol
+            </button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
